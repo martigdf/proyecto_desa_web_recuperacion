882b3e4716ef1336cff5add1d113289f3045750a
clara-s-n <ana.sena@correo.ucu.edu.uy>
2024-09-23 10:55:57 -0300

    Plugins y actualización de fastify

---
+++ backend/src/plugins/jwt.ts
@@ -0,0 +1,22 @@
+import jwt, { FastifyJWTOptions } from "@fastify/jwt";
+import fp from "fastify-plugin";
+import { FastifyReply, FastifyRequest } from "fastify";
+import { authenticateFunction } from '../types/fastify.js';
+
+const jwtOptions: FastifyJWTOptions = {
+    secret: 'supersecret'
+};
+
+export default fp(async (fastify) => {
+    fastify.register(jwt, jwtOptions);
+
+    const authenticate: authenticateFunction = async (request: FastifyRequest, reply: FastifyReply) => {
+        try {
+            await request.jwtVerify();
+        } catch (err) {
+            reply.code(401).send({ error: 'Unauthorized' });
+        }
+    };
+
+    fastify.decorate("authenticate", authenticate);
+});
--- backend/src/plugins/sensible.ts
+++ backend/src/plugins/sensible.ts
@@ -1,11 +1,8 @@
-import fp from 'fastify-plugin'
-import sensible, { SensibleOptions } from '@fastify/sensible'
+import { FastifyPluginAsync } from 'fastify';
+import sensible from '@fastify/sensible';
 
-/**
- * This plugins adds some utilities to handle http errors
- *
- * @see https://github.com/fastify/fastify-sensible
- */
-export default fp<SensibleOptions>(async (fastify) => {
-  fastify.register(sensible)
-})
+const plugin: FastifyPluginAsync = async (fastify, options) => {
+  fastify.register(sensible);
+};
+
+export default plugin;
