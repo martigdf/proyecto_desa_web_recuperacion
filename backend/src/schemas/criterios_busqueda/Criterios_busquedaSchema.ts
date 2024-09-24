a2ef4b7ecc332d7465b8f8c056b63221fc0425df
martigdf <martigdf@LAPTOP-T038RBSK>
2024-09-24 17:02:03 -0300

    dos schemas en propiedad ubicacion/datos de contacto

--- backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
+++ backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
@@ -1,6 +1,6 @@
 import { Static, Type } from "@sinclair/typebox";
 import { UsuarioSchema } from "../usuario/UsuarioSchema.js";
-
+import { UbicacionSchema } from '../propiedades/PropiedadSchema.js';
 
 const PropertyTypeEnum = Type.Union([
     Type.Literal("apartmento"),
@@ -10,11 +10,11 @@ const PropertyTypeEnum = Type.Union([
 export const Criterios_busquedaSchema = Type.Object({
     id: Type.Number(),
     userId: Type.Ref(UsuarioSchema.properties.id),
-    location: Type.String({ minLength: 2, maxLength: 100 }),
+    location: UbicacionSchema,
     price_rangeMin: Type.Optional(Type.Number({ minimum: 0 })),
     price_rangeMax: Type.Optional(Type.Number({ minimum: 0 })),
     number_rooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10 })),
-    property_type: Type.Optional(PropertyTypeEnum),
+    property_type: PropertyTypeEnum,
 }, { additionalProperties: false });
 
 export type CriteriosDeBusquedaType = Static<typeof Criterios_busquedaSchema>;
--- backend/src/schemas/propiedades/PropiedadSchema.ts
+++ backend/src/schemas/propiedades/PropiedadSchema.ts
@@ -1,18 +1,32 @@
 import { Static, Type } from "@sinclair/typebox";
 
+//Ubicación
+export const UbicacionSchema = Type.Object({
+    address: Type.String({ minLength: 2, maxLength: 100 }),
+    city: Type.String({ minLength: 2, maxLength: 50 }),
+    country: Type.String({ minLength: 2, maxLength: 50 }),
+}, { additionalProperties: false });
+
+//Datos de contacto
+const ContactDetailsSchema = Type.Object({
+    name: Type.String({ minLength: 2, maxLength: 100 }),
+    email: Type.String({ format: 'email' }),
+    phone: Type.String({ minLength: 7, maxLength: 15 }),
+}, { additionalProperties: false });
+
 
 export const PropiedadSchema = Type.Object({
     id: Type.Number(),
     title: Type.String({ minLength: 2, maxLength: 100 }),
-    description: Type.Optional(Type.String()),
+    description: Type.Optional(Type.String({ maxLength: 500 })),
     price: Type.Number({ minimum: 0 }),
-    location: Type.String({ minLength: 2, maxLength: 100 }),
-    area: Type.Optional(Type.Number({ minimum: 0, maximum: 1000})),
-    number_rooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10 })),
-    number_bathrooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10})),
+    location: UbicacionSchema,
+    area: Type.Optional(Type.Number({ minimum: 0, maximum: 1000 })),
+    number_rooms: Type.Number({ minimum: 1, maximum: 10 }),
+    number_bathrooms: Type.Number({ minimum: 1, maximum: 10 }),
     publication_date: Type.String({ format: 'date-time' }),
     //image: Type.Optional(Type.String({  })),
-    contact_details: Type.Optional(Type.String({ maxLength: 255 })),
+    contact_details: ContactDetailsSchema,
 }, { additionalProperties: false });
 
 export type PropertyType = Static<typeof PropiedadSchema>;
