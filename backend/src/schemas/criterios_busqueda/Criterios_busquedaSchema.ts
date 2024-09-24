ac8d8723e0bb9ec89c13b6b826b97043f945d265
martigdf <martigdf@LAPTOP-T038RBSK>
2024-09-24 14:50:32 -0300

    cambie minimo de cuartos y baños

--- backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
+++ backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
@@ -7,7 +7,7 @@ export const Criterios_busquedaSchema = Type.Object({
     location: Type.String({ minLength: 2, maxLength: 100 }),
     price_rangeMin: Type.Optional(Type.Number({ minimum: 0 })),
     price_rangeMax: Type.Optional(Type.Number({ minimum: 0 })),
-    number_rooms: Type.Optional(Type.Number({ minimum: 0 })),
+    number_rooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10 })),
     property_type: Type.Optional(Type.String({ minLength: 2, maxLength: 50 })),
 }, { additionalProperties: false });
 
