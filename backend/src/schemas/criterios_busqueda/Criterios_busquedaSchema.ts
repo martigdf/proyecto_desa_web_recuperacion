f527f9f2049ddf87524b6ace8ca462154d8662ab
martigdf <martigdf@LAPTOP-T038RBSK>
2024-09-24 16:09:08 -0300

    tipo de propiedad apartamento/casa y cambie email en usuario

--- backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
+++ backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
@@ -1,6 +1,12 @@
 import { Static, Type } from "@sinclair/typebox";
 import { UsuarioSchema } from "../usuario/UsuarioSchema.js";
 
+
+const PropertyTypeEnum = Type.Union([
+    Type.Literal("apartmento"),
+    Type.Literal("casa"),
+]);
+
 export const Criterios_busquedaSchema = Type.Object({
     id: Type.Number(),
     userId: Type.Ref(UsuarioSchema.properties.id),
@@ -8,7 +14,7 @@ export const Criterios_busquedaSchema = Type.Object({
     price_rangeMin: Type.Optional(Type.Number({ minimum: 0 })),
     price_rangeMax: Type.Optional(Type.Number({ minimum: 0 })),
     number_rooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10 })),
-    property_type: Type.Optional(Type.String({ minLength: 2, maxLength: 50 })),
+    property_type: Type.Optional(PropertyTypeEnum),
 }, { additionalProperties: false });
 
 export type CriteriosDeBusquedaType = Static<typeof Criterios_busquedaSchema>;
--- backend/src/schemas/usuario/UsuarioSchema.ts
+++ backend/src/schemas/usuario/UsuarioSchema.ts
@@ -1,13 +1,11 @@
 import { Static, Type } from "@sinclair/typebox";
 
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
 
 export const UsuarioSchema = Type.Object({
     id: Type.Number(),
     name: Type.String({ minLength: 2, maxLength: 50 }),
     lastname: Type.String({ minLength: 2, maxLength: 50 }),
-    email: Type.String({ format: 'email', pattern: emailRegex.source }),
+    email: Type.String({ type: 'string', format: 'email' }),
     registration_date: Type.String({ format: 'date-time' }),
     role: Type.Union([Type.Literal("admin"), Type.Literal("user")]),
 }, { additionalProperties: false });
