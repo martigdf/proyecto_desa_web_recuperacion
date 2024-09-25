cd32ed9915e9e00b5559647a138b4a2fd652a772
martigdf <martigdf@LAPTOP-T038RBSK>
2024-09-25 11:25:51 -0300

    schema favorito y notificacion

--- backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
+++ backend/src/schemas/criterios_busqueda/Criterios_busquedaSchema.ts
@@ -11,9 +11,9 @@ export const Criterios_busquedaSchema = Type.Object({
     id: Type.Number(),
     userId: Type.Ref(UsuarioSchema.properties.id),
     location: UbicacionSchema,
-    price_rangeMin: Type.Optional(Type.Number({ minimum: 0 })),
-    price_rangeMax: Type.Optional(Type.Number({ minimum: 0 })),
-    number_rooms: Type.Optional(Type.Number({ minimum: 1, maximum: 10 })),
+    price_rangeMin: Type.Number({ minimum: 0 }),
+    price_rangeMax: Type.Number({ minimum: 0 }),
+    number_rooms: Type.Number({ minimum: 1, maximum: 10 }),
     property_type: PropertyTypeEnum,
 }, { additionalProperties: false });
 
