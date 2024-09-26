af22ae1a4638ac9ac04be998fffbe3387b10ed2e
clara-s-n <ana.sena@correo.ucu.edu.uy>
2024-09-26 12:48:23 -0300

    - Creado el servicio de extracción de datos y agregado al proxy y el docker-compose.yaml - Instalados plugins en el data-source - Configurado el verifyAdmin en el data-source - Creo que estaría el schema para lo que sería la tabla data-source también - Falta hacer las en este nomás

---
+++ data-source/src/types/fastify.ts
@@ -0,0 +1,16 @@
+// import fastifyOauth2 from "@fastify/oauth2";
+import {FastifyReply, FastifyRequest} from "fastify";
+
+
+export interface authenticateFunction{
+    (request: FastifyRequest, reply: FastifyReply): Promise<void>;
+}
+
+declare module "fastify" {
+    interface FastifyInstance {
+        verifyAdmin: authenticateFunction;
+        //googleOAuth2: fastifyOauth2.OAuth2Namespace
+    }
+}
+
+export default authenticateFunction;
