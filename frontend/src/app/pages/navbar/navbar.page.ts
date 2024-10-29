1a4ff9985ab2096cf7b7a2bbd0ee4e93ec7427eb
martigdf <martina.guzman@correo.ucu.edu.uy>
2024-10-29 14:48:56 -0300

    navbar termianda, verifica si es usuario y muestra favorito, solo permite ver lista de usaurios si es admin, etc

--- frontend/src/app/pages/navbar/navbar.page.ts
+++ frontend/src/app/pages/navbar/navbar.page.ts
@@ -1,17 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { NavigationEnd, Router } from '@angular/router';
+import { NgIf } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './navbar.page.html',
   styleUrl: './navbar.page.css'
 })
-export class NavbarPage {
-  constructor(private authService: AuthService) {}
+export class NavbarPage implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  currentRoute: string = '';
+  isAdmin: boolean = false;
+  isValidUser: boolean = false;
+
+  ngOnInit(): void {
+    // Asigna la ruta actual al cargar el componente
+    this.currentRoute = this.router.url;
+    console.log('Ruta inicial:', this.currentRoute);
+
+    // Verifica si el usuario es admin
+    this.isAdmin = this.authService.isAdmin();
+    console.log('Es admin:', this.isAdmin);
+
+    // Verifica si el usuario está logueado
+    this.isValidUser = this.authService.isValidUser();
+    console.log('Usuario válido:', this.isValidUser);
+  
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Actualiza la ruta en cada cambio de navegación
+        this.currentRoute = event.url;
+        console.log('Ruta actualizada:', this.currentRoute);
+      }
+    });
+  }
+
+  isActive(routes: string[]): boolean {
+    const filteredRoutes = routes.filter(route => route !== this.currentRoute);
+    const isMatch = filteredRoutes.some(route => this.currentRoute === route);
+    console.log('Verificando rutas:', filteredRoutes, 'Ruta actual:', this.currentRoute, 'Coincide:', isMatch);
+    return isMatch;
+  }
+  
   logout() {
     this.authService.logout();
   }
