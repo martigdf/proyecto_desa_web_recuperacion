fc143a813b681639e23657d8e3eae50db16fb16e
martigdf <martina.guzman@correo.ucu.edu.uy>
2024-10-29 08:27:10 -0300

    me parece que quedaria bien asi idk

--- frontend/src/app/layout/layout.component.ts
+++ frontend/src/app/layout/layout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarPage } from "../pages/navbar/navbar.page";
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [NavbarPage],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
